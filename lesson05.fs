0a67400c49bfa5eff3a096b02b38bd1de5686d67
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2026-09-21 16:34:07 +0400

    Update lesson05.fs

--- lesson05.fs
+++ lesson05.fs
@@ -2,4 +2,12 @@
 let notDivisible  (n, m) = m % n = 0
 
 // 16.2
-let  prime n = n 
+let  prime  = function
+| n when n < 2 -> false
+| n ->
+    let rec checkPrime = function
+    | d when d = n -> true
+    | d when n % d = 0 -> false
+    | d -> checkPrime (d + 1)
+
+    checkPrime 2
