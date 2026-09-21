cc9dbd29af0b54c81a243f85d2a569a748a056b9
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2026-09-21 20:48:31 +0400

    Update lesson06.fs

--- lesson06.fs
+++ lesson06.fs
@@ -7,4 +7,7 @@ let rec pow  = function
 let rec isIthChar (s: string, n, c) = s.[n] = c
 
 // 17.3
-let rec occFromIth  ...
+let rec occFromIth  = function
+    | (s, n, c) when n = String.length s -> 0
+    | (s, n, c) when s.[n] = c -> 1 + occFromIth(s, (n+1), c)
+    | (s, n, c) -> occFromIth(s, (n+1), c)
