100b162a89165889b8ab04de4dffd4f5b7c5eeed
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2026-09-22 21:56:25 +0400

    Update lesson07.fs

--- lesson07.fs
+++ lesson07.fs
@@ -6,3 +6,7 @@ let unvat n (x: float) : float =  x / (1.0 + float n / 100.0)
 
 // 20.3.3
 let rec min f = 
+    let rec search = function
+        | n when f n = 0 -> n 
+        | n -> search (n + 1)
+    search 1
