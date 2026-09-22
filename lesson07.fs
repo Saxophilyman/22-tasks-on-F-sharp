409fd9d356fa0ff0d1d8bc4098dcc63d38c00858
Saxophilyman <86315790+Saxophilyman@users.noreply.github.com>
2026-09-22 20:54:15 +0400

    Update lesson07.fs

--- lesson07.fs
+++ lesson07.fs
@@ -4,9 +4,5 @@ let vat n (x: float) : float = x + (x/float(100)) * float n
 // 20.3.2
 let unvat n (x: float) : float =  x / (1.0 + float n / 100.0)
 
-printfn "%f" (vat 100 50)
-printfn "%f" (unvat 100 50)
-printfn "%f" (unvat 100 (vat 100 50))
-
 // 20.3.3
 let rec min f = 
